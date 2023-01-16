e06eee8020d349429a148e20f6bd1f0695c60c25
Alexey <alexei20293@yahoo.com>
2023-01-16 21:58:25 +0200

    add(feature): verification with email logic

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,7 +1,10 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const { v4: uuidv4 } = require('uuid');
 const { User } = require('../models/userModel');
-const { RegistrationConflictError, NotAuthorizedError } = require('../helpers/customErrors');
+const { RegistrationConflictError, NotAuthorizedError, NotFoundError, BadRequestError } = require('../helpers/customErrors');
+const { sendEmail } = require('../services/sendEmail');
+const { verificationTemplate, resendVerificationTemplate } = require('../email_templates/emailTemplates')
 
 const registerController = async (req, res) => {
     const { email, password } = req.body;
@@ -10,8 +13,17 @@ const registerController = async (req, res) => {
         throw new RegistrationConflictError('This email is already in use');
     }
 
-    const user = new User({password, email});
+    const verificationToken = uuidv4();
+
+    const user = new User({ password, email, verificationToken });
     await user.save();
+    
+    const mail = {
+        to: email,
+        subject: "Verify your email address",
+        html: verificationTemplate(verificationToken)
+    }
+    await sendEmail(mail);
     return res.status(201).json({email});
 }
 
@@ -23,6 +35,9 @@ const loginController = async (req, res) => {
     }
     if(!await bcrypt.compare(password, user.password)) {
         throw new NotAuthorizedError('Wrong password');
+    }
+    if(!user.verify) {
+        throw new NotAuthorizedError('Email not verify');
     }
     
     const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
@@ -41,6 +56,39 @@ const logoutController = async (req, res) => {
     return res.status(204).json();
 }
 
+const verifyEmailController = async (req, res) => { 
+    const { verificationToken } = req.params;
+    
+    const user = await User.findOne({verificationToken});
+    if (!user) {
+        throw new NotFoundError('User not found');
+    }
+    await User.findByIdAndUpdate(user._id, { verify: true, verificationToken: "" });
+    return res.status(200).json({
+        message: "Verification successfull"
+    })
+}
+
+const resendVerifyEmailController = async (req, res) => {
+    const { email } = req.body;
+    const user = await User.findOne({ email })
+    if (!user) {
+        throw new NotFoundError('User not found');
+    };
+    if(user.verify) {
+        throw new BadRequestError('Verification has already been passed');
+    };
+    const mail = {
+        to: email,
+        subject: "Verify your email address",
+        html: resendVerificationTemplate(user.verificationToken)
+    }
+    await sendEmail(mail);
+    return res.status(200).json({
+        message: "Verification email resent"
+    })
+}
+
 module.exports = {
-    registerController, loginController, logoutController, getCurrentController
+    registerController, loginController, logoutController, getCurrentController, verifyEmailController, resendVerifyEmailController
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,13 +2,18 @@ const express = require('express');
 const router = express.Router();
 
 const { asyncWrapper } = require('../helpers/apiHelpers');
-const { registerController, loginController, logoutController, getCurrentController } = require('../controllers/userControllers');
+const { registerController, loginController, logoutController, getCurrentController, verifyEmailController, resendVerifyEmailController } = require('../controllers/userControllers');
 const { authMiddleware } = require('../middlewares/authMiddleware');
 
 
+
 router.post('/register', asyncWrapper(registerController));
+router.get('/verify/:verificationToken', asyncWrapper(verifyEmailController));
+router.post('/verify', asyncWrapper(resendVerifyEmailController));
 router.post('/login', asyncWrapper(loginController));
 router.get('/current', authMiddleware, asyncWrapper(getCurrentController));
 router.get('/logout', authMiddleware, asyncWrapper(logoutController));
+
+
 
 module.exports = router
